d067625b1059e8741caaf1b546badf538b619478
The Taha Yaseen <thetahayaseen@gmail.com>
2023-11-15 00:00:52 +0500

    Config For All Project Scenarios

--- pre_school/wsgi.stage.py
+++ pre_school/wsgi.stage.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pre_school.conf.dev')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pre_school.conf.stage')
 
 application = get_wsgi_application()
